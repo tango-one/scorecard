8441bd6c5cba0aaacffe5140448c0da74a18b1b8
Tom Christensen <36410725+tango-one@users.noreply.github.com>
2023-03-06 19:25:09 +0100

    scoreboard project

---
+++ js/score.js
@@ -0,0 +1,52 @@
+
+
+
+
+let display = document.getElementById("display-home")
+let displayAway = document.getElementById("display-guest")
+
+let countHome = 0
+let countAway = 0
+
+
+// home team
+
+
+function one() {
+    countHome = countHome + 1
+    display.innerText = countHome
+    console.log(countHome)
+}
+
+function two() {
+    countHome = countHome + 2
+    display.innerText = countHome
+    console.log(count)
+}
+
+function tre() {
+    countHome = countHome + 3
+    display.innerText = countHome
+    console.log(count)
+}
+
+
+// Guest team
+
+
+function GuestOne() {
+    countAway = countAway + 1
+    displayAway.innerText = countAway
+}
+
+function GuestTwo() {
+    countAway = countAway + 2
+    displayAway.innerText = countAway
+}
+
+function GuestTre() {
+    countAway = countAway + 3
+    displayAway.innerText = countAway
+}
+
+ 
